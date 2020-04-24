b1d8a82c350c6452909702a94525ab910a387136
Lizzy <axworthy@sonoma.edu>
2020-04-24 00:25:36 -0700

    Changed position_map to available_spaces. Removes squares as they are occupied.

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pygame as pg
 
-## TODO: Add computer moves, checking to see if square is already occupied.
+## TODO: Add computer moves.
 
 class Board():
 
     def __init__(self):
+        #initialize basic variables
         self.state = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
         self.player_char = ''
         self.complete = False
         self.last_move = -1
+        self.available_spaces = {1: (0,0), 2:(0,1), 3:(0,2), 4:(1,0), 5:(1,1), 6:(1,2), 7:(2,0), 8:(2,1), 9:(2,2)}
+        #let player select their character and set computer character accordingly
         while self.player_char != 'x' and self.player_char != 'o':
             self.player_char = input("Would you like to be x or o: ").lower()
         self.computer_char = 'o' if self.player_char == 'x' else 'x'
+        #start game
         print("Player will be {0}'s and I will be {1}'s.\nWhat is your first move?".format(self.player_char, self.computer_char))
+        self.play()
 
     def print_board(self):
         s = self.state
@@ -30,18 +35,19 @@ class Board():
             try:
                 self.print_board()
                 pos = int(input("Where would you like to move : "))
-                row = self.position_map[pos][0]
-                col = self.position_map[pos][1]
+                row = self.available_spaces[pos][0]
+                col = self.available_spaces[pos][1]
                 break
             except KeyError:
                 print("Bad space selection. Please try again.")
         self.state[row][col] = self.player_char
-        self.last_move = pos
+        del self.available_spaces[pos]
+        self.last_move = (row, col)
         return self.check_state()
 
     def check_state(self):
-        row = self.position_map[self.last_move][0]
-        col = self.position_map[self.last_move][1]
+        row = self.last_move[0]
+        col = self.last_move[1]
         s = self.state
         if s[row][0] == s[row][1] == s[row][2]:
             return True
@@ -56,7 +62,5 @@ class Board():
             self.complete = self.make_move()
         self.print_board()
 
-    position_map = {1: (0,0), 2:(0,1), 3:(0,2), 4:(1,0), 5:(1,1), 6:(1,2), 7:(2,0), 8:(2,1), 9:(2,2)}
 
 board = Board()
-board.play()
